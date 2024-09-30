b9d80f59e38e068e2132c96f62b8b6493c93f51a
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-09-30 21:42:30 +0400

    commit

--- Password generator/Pass_generator.py
+++ Password generator/Pass_generator.py
@@ -2,15 +2,20 @@ import random
 
 
 print("Welcome to the Password generator")
-
+running = True
 chars = "abcdefghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%?&0123456789"
 amount_of_pass = int(input("Enter amount of password👉: "))
 
 pass_len = int(input("Enter lenght of the password👉: "))
 
-for _ in range(amount_of_pass):
-    password = ""
-    for _ in range(pass_len):
-        password += random.choice(chars)
+while running:
+    for _ in range(amount_of_pass):
+        password = ""
+        for _ in range(pass_len):
+            password += random.choice(chars)
+        print("\n", password)
+    if not input("Want another password for you (y/n)?").lower() == "y":
+        running = False
 
-    print("\n", password)
+
+print("Thanks for using password generator ♥")
--- Rock Paper Scissors - python/Rock_Paper_Scissors.py
+++ Rock Paper Scissors - python/Rock_Paper_Scissors.py
@@ -6,9 +6,10 @@ while running:
     player = None
     
     comp = random.choice(choice)
-
+    
     player = input("Enter your choice (Rock ✊,Paper ✋, Scissors ✂): ")
-
+    comp_score = 0
+    my_score = 0
     
     while player not in choice:
         player = input("Enter your choice (Rock ✊, Paper ✋,Scissors ✂): ")
@@ -20,17 +21,22 @@ while running:
         print("It's Tie")
     elif (player == "Rock") and (comp == "Paper"):
         print("You lose")
+        comp_score += 1
     elif (player == "Paper") and (comp == "Scissors"):
         print("You lose")
+        comp_score += 1
     elif (player == "Scissors") and (comp == "Rock"):
         print("You lose")
+        comp_score += 1
     else:
         print("You win")
-
+        my_score += 1
+    print(f"💻Computer score: {comp_score}")
+    print(f"🧑Your score :{my_score}")
     
     if not input("Play again(y/n)?: ").lower() == "y":
         running = False
-print("Thanks for playing <3")
+print("Thanks for playing ♥")
 
 
 
