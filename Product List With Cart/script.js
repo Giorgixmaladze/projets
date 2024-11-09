d111e90da96f5248f249bfd0e2a240db48a81bee
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-11-10 01:52:43 +0400

    commit

--- Product List With Cart/script.js
+++ Product List With Cart/script.js
@@ -1,8 +1,10 @@
 const products = document.getElementById("products")
 const dess = document.getElementsByClassName("dess")
-let cartItems =[]
+let values =[]
 let cart = document.getElementById("cart")
 let total = document.createElement("div")
+let carbon = document.createElement("div")
+carbon.id = "delivery"
 total.id = "total"
 let count = 0
 let order = document.createElement("p")
@@ -10,6 +12,13 @@ order.textContent = "Order total"
 total.appendChild(order)
 let h3 = document.createElement("h3")
 total.appendChild(h3)
+let tree = document.createElement("img")
+tree.src = "./assets/images/icon-carbon-neutral.svg"
+let carbonNeutral = document.createElement("p")
+carbonNeutral.textContent = "This is a carbon-neutral delivery"
+carbon.appendChild(tree)
+carbon.appendChild(carbonNeutral)
+
 Array.from(dess).forEach(des=>{
     let button = des.firstElementChild.nextElementSibling.firstElementChild
     
@@ -19,14 +28,15 @@ Array.from(dess).forEach(des=>{
         let product = des.lastElementChild.firstElementChild.nextElementSibling.textContent
         let productPrice = des.lastElementChild.lastElementChild.textContent
         total.style.display = "flex"
+        carbon.style.display = "flex"
         console.log(productPrice)
-        console.log(cartItems)
+        
         let background = cart.firstElementChild.nextElementSibling;
         background.style.display = "none";
         let border = des.firstElementChild
         border.style.border = '1px solid hsl(14, 86%, 42%)'
         button.style.display = "none"   
-        
+        let divs = document.createElement("div") 
         
         
         let buttonDiv = document.createElement("div")
@@ -51,12 +61,12 @@ Array.from(dess).forEach(des=>{
         let cartP = document.createElement("p")
         let fullProductValue = document.createElement("p")
         let remove = document.createElement("P")
-        
+        fullProductValue.textContent = productPrice
 
         
 
         
-        displayItem(border,p1,p3,background,product,cartP,productPrice,fullProductValue,cartPDiv,remove,buttonDiv)
+       
         p3.id = "increment";
         p3.textContent = "+"
         cartP.textContent = `x${productCount}`
@@ -66,9 +76,9 @@ Array.from(dess).forEach(des=>{
                 
                 button.style.display = "flex"
                 button.style.border = "none"
-                cart.removeChild(cart.lastElementChild)
                 buttonDiv.style.display = "none"
                 des.firstElementChild.style.border = 'none'
+                cart.removeChild(divs)
                 
             }else{
                 productCount --
@@ -83,15 +93,19 @@ Array.from(dess).forEach(des=>{
             p2.textContent = productCount
             cartP.textContent = `x${productCount}`
             fullProductValue.textContent = `$${productPrice.slice(1,) * productCount}`
+            total.lastElementChild.textContent = fullProductValue.textContent
            
     })
     
     
-    function displayItem(border,p3,p4,background,p,cartP,productPrice,fullProductValue,cartPDiv,remove,buttonDiv){
+    displayItem(border,p1,p3,background,product,cartP,productPrice,p2,fullProductValue,cartPDiv,remove,buttonDiv)
+    
+    
+    function displayItem(border,p3,p4,background,p,cartP,productPrice,productCount,fullProductValue,cartPDiv,remove,buttonDiv){
     
     count ++
     cart.firstElementChild.firstElementChild.textContent = `Your Cart (${count})`
-    let divs = document.createElement("div") 
+    
     divs.className = "divs"
     divs.style.display ="flex"
     cart.appendChild(divs)
@@ -111,10 +125,14 @@ Array.from(dess).forEach(des=>{
     cartPDiv.appendChild(cartP)
     cartPDiv.appendChild(p2)
     cartPDiv.appendChild(fullProductValue)
+    
+   
     cartPDiv.appendChild(remove)
     remove.addEventListener("click",()=>{
+        
         cart.removeChild(divs)
-        count --
+        
+        count = count - productCount.textContent;
         cart.firstElementChild.firstElementChild.textContent = `Your Cart (${count})`
         button.style.display = "flex"
         button.style.border = "none"
@@ -130,20 +148,26 @@ Array.from(dess).forEach(des=>{
     p3.addEventListener("click",()=>{
         count--
         cart.firstElementChild.firstElementChild.textContent = `Your Cart (${count})`
+        
+        
+        
     })
+    
     p4.addEventListener("click",() =>{
         count ++
         cart.firstElementChild.firstElementChild.textContent = `Your Cart (${count})`
     })
     cart.appendChild(total)
+    cart.appendChild(carbon)
+
+   
     
     
+
     
+  
     
-    
-    
-    
-    
+   
     
     
 }
