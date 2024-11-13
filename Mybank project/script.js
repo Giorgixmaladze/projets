6c3694391a93dad31fae8500ee08c39b71fd63f7
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-11-14 02:35:15 +0400

    commit

---
+++ Mybank project/script.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector("#sidebar");
+const butt = document.querySelector("#sugg").firstElementChild;
+
+butt.addEventListener("click",()=>{
+    sidebar.style.display = "flex"
+})
