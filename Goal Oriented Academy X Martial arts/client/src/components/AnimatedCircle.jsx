b04c88aba2cb452204aec06b96c3c3c1dfffa1cc
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2025-10-09 23:00:15 +0400

    commit

---
+++ Goal Oriented Academy X Martial arts/client/src/components/AnimatedCircle.jsx
@@ -0,0 +1,11 @@
+const AnimatedCircle = () => {
+    return (
+        <div className="min-[375px]:h-[300px] min-[375px]:flex min-[375px]:items-center min-[375px]:justify-center ">
+            <div className="min-[375px]:w-[280px] border border-green-800 min-[375px]:h-[280px] rounded-[50%]  circle" >
+
+            </div>
+        </div>
+
+    )
+}
+export default AnimatedCircle
--- Goal Oriented Academy X Martial arts/client/src/components/Main.jsx
+++ Goal Oriented Academy X Martial arts/client/src/components/Main.jsx
@@ -1,7 +1,9 @@
+import AnimatedCircle from "./AnimatedCircle"
+
 const Main = () =>{
     return(
         <main className="min-[375px]:min-h-screen bg-[#030712]">
-            
+            <AnimatedCircle/>
         </main>
     )
 }
